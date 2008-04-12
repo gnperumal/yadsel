70d6396c255f43ac3c957c8167ad3258574523f0
marinho <marinho@f1c57b27-a032-0410-920c-21c98535aa7c>
2008-04-12 11:37:46 +0000

    adding missed test file for drivers

---
+++ trunk/tests/drivers/sqlite_tests.py
@@ -0,0 +1,12 @@
+"""
+>>> from yadsel.core import *
+>>> class Version1(Version):
+...     version_number = 1
+...     de up(self):
+...         
+"""
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
